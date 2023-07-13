3e808d408653fde3de01c25145f16127fe0b8720
asmo <alvarado.1999.joe@gmail.com>
2023-07-13 10:17:14 -0500

    Practice writing Arrays

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 // Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+};
+
+cats.push("Ralph");
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+};
+cats.unshift("Bob");
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+};
+cats.pop();
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+};
+cats.shift();
+
+function appendCat(name) {
+    return [...cats, name];
+};
+appendCat("Broom");
+
+function prependCat(name) {
+    return [name, ...cats,];
+};
+prependCat("Arnold");
+
+function removeLastCat() {
+    return cats.slice(0, -1);
+};
+cats.slice(0, -1);
+
+function removeFirstCat() {
+    return cats.slice(1);
+};
+cats.slice(1);
